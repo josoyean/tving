8d1643f3e236ec25558c9b8008601a3411c56c17
josoyean <dlfjswhtnals@naver.com>
2024-01-12 20:11:55 +0900

    좋아요 버튼 완료, 비디오 콘텐츠 추가, 작업중

---
+++ src/components/VideoComponert.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import "../Pages/ProgramInfor.css";
+function VideoComponert({ videoInfom, count }) {
+  console.log("videoInfom", videoInfom);
+  return (
+    <VideoBox className="video-box">
+      <img
+        className="videoPoster"
+        src={videoInfom.videoThumbnail}
+        alt="videoPoster"
+      ></img>
+      <span className="title">
+        {count}. {videoInfom.videoTitle}
+      </span>
+      <span className="inform">{videoInfom.videoinform}</span>
+    </VideoBox>
+  );
+}
+
+export default VideoComponert;
+
+const VideoBox = styled.div`
+  width: 270px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
